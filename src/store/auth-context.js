f3e681f5535d95de44d75d99ff4d4d53a6d5af4b
vibesx <lucian.paslariu@gmail.com>
2022-11-26 13:38:09 +0200

    finishing steps

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
 	token: "",
@@ -17,8 +19,27 @@ const calculateRemainingTime = (expirationTime) => {
 	return remainingDuration;
 };
 
+const retrieveStoredToken = () => {
+	const storedToken = localStorage.getItem("token");
+	const storedExpirationDate = localStorage.getItem("expirationTime");
+
+	const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+	if (remainingTime <= 60000) {
+		localStorage.removeItem("token");
+		localStorage.removeItem("expirationTime");
+		return null;
+	}
+
+	return { token: storedToken, duration: remainingTime };
+};
+
 export const AuthContextProvider = (props) => {
-	const initialToken = localStorage.getItem("token");
+	const tokenData = retrieveStoredToken();
+	let initialToken;
+	if (tokenData) {
+		initialToken = tokenData.data;
+	}
 	const [token, setToken] = useState(initialToken);
 
 	// !! converts token into a boolean (truthy or falsy) value
@@ -27,16 +48,28 @@ export const AuthContextProvider = (props) => {
 	const logoutHandler = () => {
 		setToken(null);
 		localStorage.removeItem("token");
+		localStorage.removeItem("expirationTime");
+
+		if (logoutTimer) {
+			clearTimeout(logoutTimer);
+		}
 	};
 
 	const loginHandler = (token, expirationTime) => {
 		setToken(token);
 		localStorage.setItem("token", token);
+		localStorage.setItem("expirationTime", expirationTime);
 
 		const remainingTime = calculateRemainingTime(expirationTime);
 
-		setTimeout(logoutHandler, remainingTime);
+		logoutTimer = setTimeout(logoutHandler, remainingTime);
 	};
+
+	useEffect(() => {
+		if (tokenData) {
+			logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+		}
+	}, [tokenData]);
 
 	const contextValue = {
 		token: token,
